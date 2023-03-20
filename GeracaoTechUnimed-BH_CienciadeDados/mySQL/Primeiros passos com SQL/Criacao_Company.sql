0debaeebb48bf29ea638b88c144b91a182492518
Saulo Carvalho <saulodias60@gmail.com>
2023-03-20 17:23:04 -0300

    inserção de dados bd company

--- GeracaoTechUnimed-BH_CienciadeDados/mySQL/Primeiros passos com SQL/Criacao_Company.sql
+++ GeracaoTechUnimed-BH_CienciadeDados/mySQL/Primeiros passos com SQL/Criacao_Company.sql
@@ -59,13 +59,14 @@ create table works_on(
     constraint fk_project_works_on foreign key (Pno) references project (Pnumber),
     constraint fk_employee_works_on foreign key (Essn) references employee (Ssn)
 );
-
 create table dependent(
 	Essn char(9) not null,
     Dependent_name varchar(15) not null,
     Sex char,
     Bdate date,
     Relationship varchar(8),
+    Age int not null,
+    constraint chk_age_dependent check (Age < 22),
     primary key (Essn, Dependent_name),
     constraint fk_dependent foreign key (Essn) references employee (Ssn)
 );
