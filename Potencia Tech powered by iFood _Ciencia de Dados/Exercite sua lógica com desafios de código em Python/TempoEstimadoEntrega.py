78b66341bc77a7ec48baf60f6d4636bda588deec
Saulo Carvalho <saulodias60@gmail.com>
2024-01-11 16:57:15 -0300

    Primeira tentativa do primeiro desafio

---
+++ Potencia Tech powered by iFood _Ciencia de Dados/Exercite sua lógica com desafios de código em Python/TempoEstimadoEntrega.py
@@ -0,0 +1,6 @@
+#nomeRestaurante = input("Insira o nome do restaurante")
+#tempoEstimadoEntrega = int(input("Insira o tempo estimado de entrega em minutos"))
+nomeRestaurante = input()
+tempoEstimadoEntrega = int(input())
+
+print(f'O restaurante {nomeRestaurante} entrega em {tempoEstimadoEntrega} minutos.')
