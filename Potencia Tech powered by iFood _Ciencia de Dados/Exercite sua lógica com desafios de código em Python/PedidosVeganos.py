dcc2893eae0b958191f46de17a37c5903941dae1
Saulo Carvalho <saulodias60@gmail.com>
2024-01-12 16:53:58 -0300

    último desafio

--- Potencia Tech powered by iFood _Ciencia de Dados/Exercite sua lógica com desafios de código em Python/PedidosVeganos.py
+++ Potencia Tech powered by iFood _Ciencia de Dados/Exercite sua lógica com desafios de código em Python/PedidosVeganos.py
@@ -4,9 +4,10 @@ for i in range(1, numPedidos + 1):
     prato = input()
     calorias = int(input())
     ehVegano = False
+    ehVegano=input()
+    if ehVegano =='s':
+        ehVegano='Vegano'
 
-ehVegano=input()
-if ehVegano =='s':
-    ehVegano=True
-
-print(prato, calorias, ehVegano)
+    elif ehVegano=='n':
+        ehVegano=('Nao-vegano')
+    print(f'Pedido {i}: {prato} ({ehVegano}) - {calorias} calorias')
