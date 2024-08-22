803fdba6279c9c39d668cee0c93b4b72fc8e854e
unknown <saulodias60@gmail.com>
2024-08-21 22:33:01 -0300

    Desafio de código - Explorando Técnicas de POO com Python

--- NTT DATA - Engenharia de Dados com Python/Programação Orientada a objetos com Python/CriandoClassesParaDadosDeVendas.py
+++ NTT DATA - Engenharia de Dados com Python/Programação Orientada a objetos com Python/CriandoClassesParaDadosDeVendas.py
@@ -11,14 +11,17 @@ class Relatorio:
     def adicionar_venda(self, venda):
         # TODOS: Verifique se o objeto passado é uma instância da classe Venda.
         # Isso ajuda a garantir que apenas vendas válidas sejam adicionadas ao relatório.
-        
+        if isinstance(venda, Venda):
+            self.vendas.append(venda)
+        else:
+            raise TypeError("Erro")
 
     def calcular_total_vendas(self):
         total = 0
         for venda in self.vendas:
             # TODOS: Calcule o total de vendas baseado nas vendas adicionadas:
              # O cálculo deve multiplicar a quantidade pelo valor de cada venda e somar ao total.
-            
+            total += venda.quantidade * venda.valor
         return total
 
 
@@ -34,7 +37,8 @@ def main():
     
     # TODOS: Exiba o total de vendas usando o método calcular_total_vendas.
     # Utilize o método `calcular_total_vendas` da classe `Relatorio` para mostrar o total acumulado das vendas.
-    
+    total_vendas = relatorio.calcular_total_vendas()
+    print(f"Total de Vendas: {total_vendas:.1f}")
 
 
 if __name__ == "__main__":
